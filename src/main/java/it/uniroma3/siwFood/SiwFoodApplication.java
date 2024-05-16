41c4c7798626e917deda852ed430d7cb4752f5a8
ale0699_PCDesktop <atulli030@gmail.com>
2024-05-16 20:19:09 +0200

    progetto settato

---
+++ src/main/java/it/uniroma3/siwFood/SiwFoodApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siwFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siwFood/SiwFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siwFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
