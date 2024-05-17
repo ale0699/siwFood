7d235024200ae1ac49c5547cb264e171f8ab3692
ale0699_PCDesktop <atulli030@gmail.com>
2024-05-17 22:20:21 +0200

    Update AuthConfiguration.java

--- src/main/java/it/uniroma3/siwFood/security/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siwFood/security/AuthConfiguration.java
@@ -1,16 +1,34 @@
 package it.uniroma3.siwFood.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class AuthConfiguration {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication()
+		.dataSource(dataSource)
+		.authoritiesByUsernameQuery("SELECT username, ruolo from credentials WHERE username=?")
+		.usersByUsernameQuery("SELECT username, password, 1 as enabled FROM credentials WHERE username=?");
+	}
+
 	
 	@Bean
 	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -30,4 +48,11 @@ public class AuthConfiguration {
 		
 		return http.build();
 	}
+	
+	@Bean
+	protected PasswordEncoder passwordEncoder(){
+		
+		return new BCryptPasswordEncoder();
+	}
+
 }
