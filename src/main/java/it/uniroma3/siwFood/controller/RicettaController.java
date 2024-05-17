76f8aff90e4acec6b8b1be53522f7f7eb15d5dd1
ale0699_PCDesktop <atulli030@gmail.com>
2024-05-17 12:19:44 +0200

    aggiunta la possibilità di vedere tutte le ricette

--- src/main/java/it/uniroma3/siwFood/controller/RicettaController.java
+++ src/main/java/it/uniroma3/siwFood/controller/RicettaController.java
@@ -2,6 +2,8 @@ package it.uniroma3.siwFood.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import it.uniroma3.siwFood.service.RicettaService;
 
@@ -10,4 +12,10 @@ public class RicettaController {
 	
 	@Autowired
 	private RicettaService ricettaService;
+	
+	@GetMapping(value = "/recipes")
+	public String getRecipes(Model model) {
+		model.addAttribute("recipes", this.ricettaService.findAllRicepes());
+		return "ricette/recipes.html";
+	}
 }
--- src/main/java/it/uniroma3/siwFood/service/RicettaService.java
+++ src/main/java/it/uniroma3/siwFood/service/RicettaService.java
@@ -1,8 +1,11 @@
 package it.uniroma3.siwFood.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import it.uniroma3.siwFood.model.Ricetta;
 import it.uniroma3.siwFood.repository.RicettaRepository;
 
 @Service
@@ -10,4 +13,9 @@ public class RicettaService {
 	
 	@Autowired
 	private RicettaRepository ricettaRepository;
+	
+	public List<Ricetta> findAllRicepes(){
+		
+		return (List<Ricetta>) this.ricettaRepository.findAll();
+	}
 }
