8e154f40593df0605d3130ff322cfbc9f53a9aa0
ale0699_PCDesktop <atulli030@gmail.com>
2024-05-19 11:51:38 +0200

    update url

--- src/main/java/it/uniroma3/siwFood/controller/IngredientController.java
+++ src/main/java/it/uniroma3/siwFood/controller/IngredientController.java
@@ -1,6 +1,8 @@
 package it.uniroma3.siwFood.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import it.uniroma3.siwFood.model.Credentials;
 import it.uniroma3.siwFood.model.Ingredient;
 import it.uniroma3.siwFood.model.Recipe;
+import it.uniroma3.siwFood.service.CredentialsService;
 import it.uniroma3.siwFood.service.IngredientService;
 import it.uniroma3.siwFood.service.RecipeService;
 
@@ -22,6 +26,9 @@ public class IngredientController {
 	@Autowired
 	private RecipeService recipeService;
 	
+	@Autowired
+	private CredentialsService credentialsService;
+	
 	@GetMapping(value = {"/cook/formAddIngredientRecipe/{idRecipe}", "/admin/formAddIngredientRecipe/{idRecipe}"})
 	public String getformAddIngredientRecipe(@PathVariable("idRecipe")Long idRecipe, Model model) {
 		model.addAttribute("ingredient", new Ingredient());
@@ -32,10 +39,16 @@ public class IngredientController {
 	
 	@PostMapping(value = {"/cook/addIngredient/{idRecipe}", "/admin/addIngredient/{idRecipe}"})
 	public String postAddIngredientRecipe(@ModelAttribute Ingredient ingrediente, @PathVariable("idRecipe")Long idRecipe) {
+		UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Credentials credentials = this.credentialsService.findCredenzialiByUsername(userDetails.getUsername());
 		Recipe recipe = this.recipeService.findRecipeById(idRecipe);
 		ingrediente.setRecipe(recipe);
 		this.ingredientService.saveIngredient(ingrediente);
-		return "redirect:/formAddIngredientRecipe/"+idRecipe;
+		if(credentials.getRole().equals("ADMIN")) {
+			
+			return "redirect:/admin/formAddIngredientRecipe/"+idRecipe;
+		}
+		return "redirect:/cook/formAddIngredientRecipe/"+idRecipe;
 	}
 	
 	@GetMapping(value = {"/cook/removeIngredient/{idIngredient}/{idRecipe}", "/admin/removeIngredient/{idIngredient}/{idRecipe}"})
