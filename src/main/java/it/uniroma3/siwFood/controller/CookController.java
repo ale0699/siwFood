f407f3e05f764c1e4f2ac6d27c4b40bbe5b394db
ale0699_PCLaptop <atulli030@gmail.com>
2024-05-23 10:12:32 +0200

    admin può aggiungere cuochi

--- src/main/java/it/uniroma3/siwFood/controller/CookController.java
+++ src/main/java/it/uniroma3/siwFood/controller/CookController.java
@@ -1,18 +1,14 @@
 package it.uniroma3.siwFood.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import it.uniroma3.siwFood.model.Credentials;
 import it.uniroma3.siwFood.model.Cook;
-import it.uniroma3.siwFood.service.CredentialsService;
 import it.uniroma3.siwFood.service.CookService;
-import it.uniroma3.siwFood.service.RecipeService;
 
 @Controller
 public class CookController {
@@ -20,6 +16,16 @@ public class CookController {
 	@Autowired
 	private CookService cookService;
 
-
+	@GetMapping(value = "/admin/formAddCook")
+	public String getFormAddCook(Model model) {
+		model.addAttribute("cook", new Cook());
+		return "cooks/formAddCook.html";
+	}
+	
+	@PostMapping(value = "/admin/addCook")
+	public String getAddCook(@ModelAttribute Cook cook, Model model) {
+		this.cookService.saveCook(cook);
+		return "redirect:cooks";
+	}
 
 }
